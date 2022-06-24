7c27252f1fe8c3e4e60fc0fa23e4202b68e98b7f
Yongfeng LI <wliyongfeng@gmail.com>
2022-06-24 20:21:47 +0800

    Add business logger

--- src/logger.js
+++ src/logger.js
@@ -4,10 +4,12 @@ const logLevel = process.env.LOG_LEVEL || "debug";
 const isProduction = process.env.NODE_ENV === "production";
 
 const scanFileCategory = "scan";
+const businessCategory = "business";
 
 log4js.configure({
   appenders: {
     [scanFileCategory]: { type: "file", filename: `log/scan.log` },
+    [businessCategory]: { type: "file", filename: `log/bus.log` },
     errorFile: {
       type: "file",
       filename: `log/errors.log`,
@@ -24,11 +26,17 @@ log4js.configure({
       appenders: [isProduction ? scanFileCategory : "out", "errors"],
       level: logLevel,
     },
+    [businessCategory]: {
+      appenders: [isProduction ? businessCategory : "out", "errors"],
+      level: logLevel,
+    },
   },
 });
 
 const logger = log4js.getLogger(scanFileCategory);
+const busLogger = log4js.getLogger(businessCategory);
 
 module.exports = {
   logger,
+  busLogger,
 };
