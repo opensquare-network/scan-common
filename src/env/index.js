a5c22cf940dc5e8a4d88b9ce231df32d587fa1a2
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-05 22:43:41 +0800

    Improve scan known heights checking

--- src/env/index.js
+++ src/env/index.js
@@ -1,6 +1,6 @@
 const { CHAINS } = require("../consts/chains");
 const scanStep = parseInt(process.env.SCAN_STEP) || 100;
-const useKnownHeights = !!process.env.USE_KNOWN_HEIGHTS;
+const useKnownHeights = parseInt(process.env.USE_KNOWN_HEIGHTS) === 1;
 const useMetaDb = parseInt(process.env.USE_META) === 1;
 
 let chain = null;
