0d3a5c7f28b6a732584cdc56739398bd885d2da1
Yongfeng LI <wliyongfeng@gmail.com>
2022-09-08 23:56:24 +0800

    Remove scan script

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -1,13 +1,11 @@
 const known = require("./knownHeight");
 const meta = require("./meta");
-const scan = require("./scan");
 const common = require("./common");
 const scanDb = require("./scanDb");
 
 module.exports = {
   known,
   meta,
-  scan,
   common,
   ...scanDb,
 }
