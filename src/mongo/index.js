3426796fd489cf0b0641653c6bf70f8e1ce5f2ad
Yongfeng LI <wliyongfeng@gmail.com>
2022-09-08 23:03:56 +0800

    Add ScanDb class

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -2,10 +2,12 @@ const known = require("./knownHeight");
 const meta = require("./meta");
 const scan = require("./scan");
 const common = require("./common");
+const scanDb = require("./scanDb");
 
 module.exports = {
   known,
   meta,
   scan,
   common,
+  ...scanDb,
 }
---
+++ src/mongo/scanDb.js
@@ -0,0 +1,64 @@
+const { MongoClient } = require("mongodb");
+const { getCollection } = require("./common");
+
+const statusColName = "status";
+const genesisHeight = 1;
+const mainScanName = "main-scan-height";
+
+class ScanDb {
+  #client;
+  #db;
+  #dbName;
+  #clientInitPromise;
+  #statusCol;
+
+  constructor(mongoUrl, dbName) {
+    this.#clientInitPromise = MongoClient.connect(mongoUrl, {
+      useUnifiedTopology: true,
+    });
+
+    this.#dbName = dbName;
+  }
+
+  async init() {
+    this.#client = await this.#clientInitPromise;
+    this.#db = this.#client.db(this.#dbName);
+    this.#statusCol = await getCollection(this.#db, statusColName);
+  }
+
+  #checkInit() {
+    if (!this.#db) {
+      throw new Error(`Database is not initialized`)
+    }
+  }
+
+  async createCol(colName) {
+    this.#checkInit();
+    return await getCollection(this.#db, colName);
+  }
+
+  async getNextScanHeight() {
+    const heightInfo = await this.#statusCol.findOne({ name: mainScanName });
+
+    if (!heightInfo) {
+      return genesisHeight;
+    } else if (typeof heightInfo.value === "number") {
+      return heightInfo.value + 1;
+    } else {
+      console.error("Scan height value error in DB!");
+      process.exit(1);
+    }
+  }
+
+  async updateScanHeight(height) {
+    await this.#statusCol.updateOne(
+      { name: mainScanName },
+      { $set: { value: height } },
+      { upsert: true }
+    );
+  }
+}
+
+module.exports = {
+  ScanDb,
+}
