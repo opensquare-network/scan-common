b2de580e0bcd899b3792741b1c678f63f0faa082
Yongfeng LI <wliyongfeng@gmail.com>
2022-09-10 20:57:25 +0800

    Add function to subscribe unFinalized height

--- src/chain/latestHead.js
+++ src/chain/latestHead.js
@@ -1,6 +1,7 @@
 const { getApi } = require("./api");
 
 let latestHeight = null;
+let latestUnFinalizedHeight = null;
 
 async function subscribeChainHeight() {
   const api = await getApi();
@@ -13,11 +14,35 @@ async function subscribeChainHeight() {
   });
 }
 
+// latest un-finalized block height
+async function subscribeLatestHeight() {
+  const api = await getApi();
+
+  await new Promise((resolve) => {
+    api.rpc.chain.subscribeNewHeads((header) => {
+      latestUnFinalizedHeight = header.number.toNumber();
+      resolve();
+    });
+  });
+}
+
+function getLatestUnFinalizedHeight() {
+  return latestUnFinalizedHeight;
+}
+
+function getLatestFinalizedHeight() {
+  return latestHeight;
+}
+
 function getLatestHeight() {
   return latestHeight;
 }
 
 module.exports = {
   subscribeChainHeight,
+  subscribeFinalizedHeight: subscribeChainHeight,
+  subscribeLatestHeight,
+  getLatestUnFinalizedHeight,
+  getLatestFinalizedHeight,
   getLatestHeight,
 };
