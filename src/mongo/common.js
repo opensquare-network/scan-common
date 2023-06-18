9c260239f1896ded4356e88ca3103effd199a41a
Yongfeng LI <wliyongfeng@gmail.com>
2023-06-18 14:45:15 +0800

    Improve mongodb getCollection

--- src/mongo/common.js
+++ src/mongo/common.js
@@ -3,18 +3,7 @@ async function getCollection(db, colName) {
     throw new Error(`Call getCollection after db is initialized`);
   }
 
-  return new Promise((resolve, reject) => {
-    db.listCollections({ name: colName }).next(async (err, info) => {
-      if (!info) {
-        const col = await db.createCollection(colName);
-        resolve(col);
-      } else if (err) {
-        reject(err);
-      }
-
-      resolve(db.collection(colName));
-    });
-  });
+  return db.collection(colName);
 }
 
 module.exports = {
